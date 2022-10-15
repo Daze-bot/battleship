56c039f358b061dd9b9f393240b8c76aa3d4f9f3
Code4Daze <jevon.fillette@gmail.com>
2022-10-15 17:19:21 -0400

    Create and test Gameboard methods

--- src/__tests__/Gameboard.test.js
+++ src/__tests__/Gameboard.test.js
@@ -1,1 +1,82 @@
 import { Gameboard } from "../scripts/Gameboard";
+import { Ship } from "../scripts/Ship";
+
+test('2D array test', () => {
+  let test = new Gameboard('test');
+  test.board[3][5] = 'hello';
+  expect(test.board[3][5]).toEqual('hello');
+})
+
+test('ship placement', () => {
+  let newBoard = new Gameboard('test');
+  newBoard.placeShip(5, 9, 'testShip');
+  expect(newBoard.board[5][9]).toBe('testShip');
+})
+
+test('ship class placement', () => {
+  let newBoard = new Gameboard('test');
+  let newShip = new Ship('testShip', 3);
+  newBoard.placeShip(8, 2, newShip);
+  expect(newBoard.board[8][2]).toEqual(newShip);
+})
+
+test('receive attack', () => {
+  let newBoard = new Gameboard('test');
+  let newShip = new Ship('testShip', 3);
+  newBoard.placeShip(8, 2, newShip);
+  newBoard.receiveAttack(8, 2);
+  expect(newShip.checkHealth()).toBe(2);
+})
+
+test('multiple attacks on same ship - different coords', () => {
+  let newBoard = new Gameboard('test');
+  let newShip = new Ship('testShip', 3);
+  newBoard.placeShip(8, 1, newShip);
+  newBoard.placeShip(8, 2, newShip);
+  newBoard.placeShip(8, 3, newShip);
+  newBoard.receiveAttack(8, 1);
+  newBoard.receiveAttack(8, 2);
+  newBoard.receiveAttack(8, 3);
+  expect(newShip.isSunk).toBe(true);
+})
+
+test('misses', () => {
+  let newBoard = new Gameboard('test');
+  let newShip = new Ship('testShip', 3);
+  newBoard.placeShip(8, 1, newShip);
+  newBoard.receiveAttack(8, 2);
+  expect(newBoard.board[8][2]).toBe("miss");
+})
+
+test('miss log', () => {
+  let newBoard = new Gameboard('test');
+  let newShip = new Ship('testShip', 3);
+  newBoard.placeShip(8, 1, newShip);
+  newBoard.receiveAttack(8, 2);
+  newBoard.receiveAttack(8, 1);
+  newBoard.receiveAttack(2, 7);
+  expect(newBoard.reportMisses()).toEqual([(2, 7), (8, 2)]);
+})
+
+test('check all NOT sunk', () => {
+  let newBoard = new Gameboard('test');
+  let newShip = new Ship('testShip', 3);
+  newBoard.placeShip(8, 1, newShip);
+  newBoard.placeShip(8, 2, newShip);
+  newBoard.placeShip(8, 3, newShip);
+  newBoard.receiveAttack(8, 1);
+  newBoard.receiveAttack(8, 2);
+  expect(newBoard.checkAllShipsSunk()).toBe(false);
+})
+
+test('check all sunk', () => {
+  let newBoard = new Gameboard('test');
+  let newShip = new Ship('testShip', 3);
+  newBoard.placeShip(8, 1, newShip);
+  newBoard.placeShip(8, 2, newShip);
+  newBoard.placeShip(8, 3, newShip);
+  newBoard.receiveAttack(8, 1);
+  newBoard.receiveAttack(8, 2);
+  newBoard.receiveAttack(8, 3);
+  expect(newBoard.checkAllShipsSunk()).toBe(true);
+})
--- src/scripts/Gameboard.js
+++ src/scripts/Gameboard.js
@@ -1,5 +1,61 @@
 class Gameboard {
-  
+  constructor(player) {
+    this.player = player;
+    this.board = [];
+    for (let i = 0; i < 10; i++) {
+      this.board.push(["", "", "", "", "", "", "", "", "", ""]);
+    }
+  }
+
+  placeShip(column, row, ship) {
+    this.board[column][row] = ship;
+  }
+
+  receiveAttack(column, row) {
+    if (this.board[column][row] !== "") {
+      this.board[column][row].hit();
+      this.board[column][row] = 'hit';
+    } else {
+      this.board[column][row] = 'miss';
+    }
+  }
+
+  reportMisses() {
+    let missLog = [];
+    for (let i = 0; i < this.board.length; i++) {
+      for (let j = 0; j < this.board[i].length; j++) {
+        if (this.board[i][j] === 'miss') {
+          missLog.push((i, j));
+        }
+      }
+    }
+    return missLog;
+  }
+
+  reportHits() {
+    let hitLog = [];
+    for (let i = 0; i < this.board.length; i++) {
+      for (let j = 0; j < this.board[i].length; j++) {
+        if (this.board[i][j] === 'hit') {
+          hitLog.push((i, j));
+        }
+      }
+    }
+    return hitLog;
+  }
+
+  checkAllShipsSunk() {
+    for (let i = 0; i < this.board.length; i++) {
+      for (let j = 0; j < this.board[i].length; j++) {
+        if (this.board[i][j] !== 'hit' &&
+            this.board[i][j] !=='miss' &&
+            this.board[i][j] !== "") {
+          return false;
+        }
+      }
+    }
+    return true;
+  }
 }
 
 export { Gameboard };
