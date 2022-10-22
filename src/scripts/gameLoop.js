aad6cca4d2ce1dd5a69001f8e0ecbee3e683f9b5
Code4Daze <jevon.fillette@gmail.com>
2022-10-21 22:01:49 -0400

    Make clicks not work if guessed

--- src/scripts/gameLoop.js
+++ src/scripts/gameLoop.js
@@ -18,8 +18,13 @@ function gameLoop() {
     square.addEventListener('click', () => {
       let squareColumn = square.id.slice(2, 3);
       let squareRow = square.id.slice(1, 2);
+      if (computerPlayer.gameboard.notYetGuessed(squareColumn, squareRow) === false) {
+        return;
+      }
+
       userPlayer.makeAttack(squareColumn, squareRow, computerPlayer.gameboard)
       computerPlayer.renderOpponentBoard();
+
       let randomColumn = Math.floor(Math.random() * 10);
       let randomRow = Math.floor(Math.random() * 10);
       computerPlayer.makeAttack(randomColumn, randomRow, userPlayer.gameboard);
