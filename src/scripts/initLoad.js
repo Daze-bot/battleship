86a6282b7f0f6ebc41b558bdfc4481450fd6f8e7
Code4Daze <jevon.fillette@gmail.com>
2022-10-29 17:29:35 -0400

    Allow game to be started with 'Enter'

--- src/scripts/initLoad.js
+++ src/scripts/initLoad.js
@@ -8,20 +8,29 @@ function initLoad() {
   document.body.appendChild(customAlertScreen());
 
   let startGame = document.querySelector('.startGame');
-  startGame.addEventListener('click', () => {
-    let playerName = document.querySelector('#userPlayerName').value;
-    if (playerName === "") {
-      playerName = 'User Player';
+  startGame.addEventListener('click', () => initiateGame());
+
+  let nameInput = document.querySelector('#userPlayerName');
+  nameInput.addEventListener('keypress', (e) => {
+    if (e.key === 'Enter') {
+      initiateGame();
     }
-
-    window.localStorage.setItem('battleshipPlayerName', `${playerName}`);
-
-    let newGameOptions = document.querySelector('.newGameOptions');
-    document.body.removeChild(newGameOptions);
-
-    gameLoad(playerName);
-    gameLoop(playerName);
   })
+}
+
+function initiateGame() {
+  let playerName = document.querySelector('#userPlayerName').value;
+  if (playerName === "") {
+    playerName = 'User Player';
+  }
+
+  window.localStorage.setItem('battleshipPlayerName', `${playerName}`);
+
+  let newGameOptions = document.querySelector('.newGameOptions');
+  document.body.removeChild(newGameOptions);
+
+  gameLoad(playerName);
+  gameLoop(playerName);
 }
 
 function createHeader() {
